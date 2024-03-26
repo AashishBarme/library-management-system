02f934e448dd0ef83f5b42640bdb030c9131bc17
AashishBarme <aashish.barme9@gmail.com>
2024-03-26 15:52:01 +0545

    remove charset from db

--- core/db.php
+++ core/db.php
@@ -37,7 +37,7 @@
             exit('Connect Error (' . mysqli_connect_errno() . ') '
                     . mysqli_connect_error());
         }
-        parent::set_charset('utf-8');
+        // parent::set_charset('utf-8');
 
        }
        
